94d5d63aa6a09f606aa36b36d742e5333699885b
Ben Cook <808@holbertonschool.com>
2020-06-11 03:34:11 -0400

    remove redundant condition from while loop

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -12,7 +12,7 @@ int check_cycle(listint_t *list)
 	if (!list)
 		return (0);
 
-	while (slow && fast && fast->next)
+	while (fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
