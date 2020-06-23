acd8cafa4b7317a6032b8476695e357bed8caa59
Ben Cook <808@holbertonschool.com>
2020-06-23 16:50:20 -0400

    generalize checking

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -15,19 +15,15 @@ def validUTF8(data):
                 return False
             trailing_bytes -= 1
         else:
+            if byte >> 6 == 0b10:
+                return False
             if byte >> 7 == 0:
                 continue
-            elif byte >> 1 == 0b1111110:
-                trailing_bytes = 5
-            elif byte >> 2 == 0b111110:
-                trailing_bytes = 4
-            elif byte >> 3 == 0b11110:
-                trailing_bytes = 3
-            elif byte >> 4 == 0b1110:
-                trailing_bytes = 2
-            elif byte >> 5 == 0b110:
-                trailing_bytes = 1
-            else:
-                return False
+
+            trailing_bytes = -1
+            mask = 0b1000000
+            while mask & byte:
+                mask >>= 1
+                trailing_bytes += 1 
 
     return trailing_bytes == 0
