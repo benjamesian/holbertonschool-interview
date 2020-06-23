c3be29b9a319bae9108c906f70d7c9659f893ea8
Ben Cook <808@holbertonschool.com>
2020-06-23 16:04:34 -0400

    remove helper function

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
 
 
-def check_tail_bytes(n, byte_array):
-    """check if n bytes start with 10 in binary"""
-    if len(byte_array) < n:
-        return False
-
-    while n:
-        byte = byte_array.pop(0)
-        if byte >> 6 != 0b10:
-            return False
-        n -= 1
-
-    return True
-
-
 def validUTF8(data):
     """check if array of UTF-8 is valid"""
-    if not data:
-        return False
-
-    data_copy = data[:]
-    while data_copy:
-        byte = data_copy.pop(0)
-        if not byte & 0b10000000:
-            continue
-        if byte >> 4 == 0b1111:
-            if not check_tail_bytes(3, data_copy):
+    trailing_bytes = 0
+    for byte in data:
+        if trailing_bytes:
+            if byte >> 6 != 0b10:
                 return False
-        elif byte >> 5 == 0b111:
-            if not check_tail_bytes(2, data_copy):
-                return False
-        elif byte >> 6 == 0b11:
-            if not check_tail_bytes(1, data_copy):
-                return False
+            trailing_bytes -= 1
         else:
-            return False
+            if not byte & 0b10000000:
+                continue
+            elif byte >> 4 == 0b1111:
+                trailing_bytes = 3
+            elif byte >> 5 == 0b111:
+                trailing_bytes = 2
+            elif byte >> 6 == 0b11:
+                trailing_bytes = 1
+            else:
+                return False
 
-    return True
+    return trailing_bytes == 0
