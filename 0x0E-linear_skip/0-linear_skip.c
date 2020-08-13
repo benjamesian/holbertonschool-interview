6655a2526e4a408d62fb268d96bc2180f9cba9ce
Ben Cook <808@holbertonschool.com>
2020-08-12 22:23:37 -0700

    fix off-by-one error

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -9,7 +9,8 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 	while ((p = p->express))
 	{
-		printf("Value checked at index [%li] = [%i]\n", p->index, p->n);
+		printf("Value checked at index [%li] = [%i]\n",
+		       p->index, p->n);
 		if (p->n >= value)
 			break;
 		list = p;
@@ -21,10 +22,12 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			break;
 		}
 	}
-	printf("Value found between indexes [%li] and [%li]\n", list->index, p->index);
-	while (list != p)
+	printf("Value found between indexes [%li] and [%li]\n",
+	       list->index, p->index);
+	while (list != p->express)
 	{
-		printf("Value checked at index [%li] = [%i]\n", list->index, list->n);
+		printf("Value checked at index [%li] = [%i]\n",
+		       list->index, list->n);
 		if (list->n == value)
 			return (list);
 		list = list->next;
