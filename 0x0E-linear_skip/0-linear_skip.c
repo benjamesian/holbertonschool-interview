05bdb00775e6107894cbdd4c3e2e2ce5da0629db
Ben Cook <808@holbertonschool.com>
2020-08-12 22:21:37 -0700

    attempt skiplist search

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,33 @@
+#include "search.h"
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *p = list;
+
+	if (!list)
+		return (NULL);
+
+	while ((p = p->express))
+	{
+		printf("Value checked at index [%li] = [%i]\n", p->index, p->n);
+		if (p->n >= value)
+			break;
+		list = p;
+
+		if (!p->express)
+		{
+			while (p->next)
+				p = p->next;
+			break;
+		}
+	}
+	printf("Value found between indexes [%li] and [%li]\n", list->index, p->index);
+	while (list != p)
+	{
+		printf("Value checked at index [%li] = [%i]\n", list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+	return (NULL);
+}
