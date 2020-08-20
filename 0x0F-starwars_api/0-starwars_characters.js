9f254c0c82fde9c1d3acd17f340c693f2948af23
Ben Cook <808@holbertonschool.com>
2020-08-20 14:10:14 -0700

    attempt task

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`, function (
+  error,
+  response,
+  body
+) {
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+  const characters = [];
+  const filmData = JSON.parse(body);
+  for (const characterUrl of filmData.characters) {
+    characters.push(
+      request(characterUrl, function (error, resp, body) {
+        return error ? null : JSON.parse(body).name;
+      })
+    );
+  }
+  Promise.all(characters).then((chars) => {
+    for (const ch of chars) {
+      console.log(ch);
+    }
+  });
+});
