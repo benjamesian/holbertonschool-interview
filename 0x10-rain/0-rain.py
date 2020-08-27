3b588371c0ec8114a269f7a59486a2be5f70ef6c
Ben Cook <808@holbertonschool.com>
2020-08-26 22:09:42 -0700

    attempt rain collection problem

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""rain collection problem"""
+
+def rain(walls):
+    """rain collected in cups challenge"""
+    out = 0
+    for i in range(1, len(walls) - 1):
+        left = max(walls[j] for j in range(i + 1))
+        right = max(walls[j] for j in range(i, len(walls)))
+        out += min(left, right) - walls[i]
+    return out
