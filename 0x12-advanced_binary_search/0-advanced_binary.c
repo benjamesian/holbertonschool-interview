73342f21418475e62d77d66bf11a2ba34c2deed0
Ben Cook <808@holbertonschool.com>
2020-09-07 21:46:56 -0700

    anotha 1

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -33,14 +33,11 @@ int helper(int *array, int value, size_t left, size_t right)
 
 	if (array[mid] == value && array[mid - 1] != value)
 		return (mid);
-	if (left == right)
-		return (-1);
 	if (array[mid] < value)
 		return (helper(array, value, mid + 1, right));
-	if (array[mid] > value)
-		return (helper(array, value, left, mid - 1));
-	else
+	if (array[mid] >= value)
 		return (helper(array, value, left, mid));
+	return (-1);
 }
 
 /**
