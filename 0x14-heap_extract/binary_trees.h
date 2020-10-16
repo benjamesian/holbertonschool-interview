97b979a042cb51550f5963aa5a58eabd53bc28bc
Ben Cook <808@holbertonschool.com>
2020-10-15 19:41:32 -0700

    add heap_t typedef

--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -20,6 +20,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *tree);
 int heap_extract(heap_t **root);
