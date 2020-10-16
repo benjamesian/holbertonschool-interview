2d8eac0a193bd17336e04463d64fa86a40a2d0d6
Ben Cook <808@holbertonschool.com>
2020-10-15 21:03:05 -0700

    return proper address when there is a loop

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -14,7 +14,15 @@ listint_t *find_listint_loop(listint_t *head)
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
 			return (slow);
+		}
 	}
 
 	return (NULL);
