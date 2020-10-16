52725fd54277753fb2f11ca601ade814b09f61f8
Ben Cook <808@holbertonschool.com>
2020-10-15 19:43:21 -0700

    betty format

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -8,12 +8,12 @@
  */
 size_t _binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    return (1 +
-            _binary_tree_size(tree->left) +
-            _binary_tree_size(tree->right));
+	return (1 +
+			_binary_tree_size(tree->left) +
+			_binary_tree_size(tree->right));
 }
 
 /**
@@ -25,11 +25,11 @@ size_t _binary_tree_size(const binary_tree_t *tree)
  */
 heap_t *find_last(heap_t *root, size_t size)
 {
-    if (size < 2)
-        return (root);
-    if (size % 2)
-        return (find_last(root, size / 2)->right);
-    return (find_last(root, size / 2)->left);
+	if (size < 2)
+		return (root);
+	if (size % 2)
+		return (find_last(root, size / 2)->right);
+	return (find_last(root, size / 2)->left);
 }
 
 /**
@@ -38,26 +38,26 @@ heap_t *find_last(heap_t *root, size_t size)
  */
 void sink(heap_t *root)
 {
-    heap_t *next = NULL;
-    int temp;
+	heap_t *next = NULL;
+	int temp;
 
-    if (root->left && root->right)
-    {
-        if (root->left->n > root->right->n && root->n < root->left->n)
-            next = root->left;
-        else if (root->n < root->right->n)
-            next = root->right;
-    }
-    else if (root->left && root->n < root->left->n)
-        next = root->left;
-    else if (root->right && root->n < root->right->n)
-        next = root->right;
-    if (!next)
-        return;
-    temp = root->n;
-    root->n = next->n;
-    next->n = temp;
-    sink(next);
+	if (root->left && root->right)
+	{
+		if (root->left->n > root->right->n && root->n < root->left->n)
+			next = root->left;
+		else if (root->n < root->right->n)
+			next = root->right;
+	}
+	else if (root->left && root->n < root->left->n)
+		next = root->left;
+	else if (root->right && root->n < root->right->n)
+		next = root->right;
+	if (!next)
+		return;
+	temp = root->n;
+	root->n = next->n;
+	next->n = temp;
+	sink(next);
 }
 
 /**
@@ -68,27 +68,27 @@ void sink(heap_t *root)
  */
 int heap_extract(heap_t **root)
 {
-    heap_t *p, *last;
-    size_t size;
-    int out = 0;
+	heap_t *p, *last;
+	size_t size;
+	int out = 0;
 
-    if (!root || !*root)
-        return (0);
-    p = *root;
-    out = p->n;
-    size = _binary_tree_size(p);
-    last = find_last(p, size);
-    if (last != p)
-    {
-        p->n = last->n;
-        if (last == last->parent->left)
-            last->parent->left = NULL;
-        else
-            last->parent->right = NULL;
-        sink(p);
-    }
-    else
-        *root = NULL;
-    free(last);
-    return (out);
+	if (!root || !*root)
+		return (0);
+	p = *root;
+	out = p->n;
+	size = _binary_tree_size(p);
+	last = find_last(p, size);
+	if (last != p)
+	{
+		p->n = last->n;
+		if (last == last->parent->left)
+			last->parent->left = NULL;
+		else
+			last->parent->right = NULL;
+		sink(p);
+	}
+	else
+		*root = NULL;
+	free(last);
+	return (out);
 }
