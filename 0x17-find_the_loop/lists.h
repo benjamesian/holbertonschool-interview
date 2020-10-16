42050d6abfee5d0c417b194cbc929977ad29c542
Ben Cook <808@holbertonschool.com>
2020-10-15 20:54:38 -0700

    add typedef

--- 0x17-find_the_loop/lists.h
+++ 0x17-find_the_loop/lists.h
@@ -3,6 +3,20 @@
 
 #include <stdlib.h>
 
+/**
+ * struct list_s - singly linked list
+ * @n: int - (data)
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H */
