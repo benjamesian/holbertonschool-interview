2dcc057d5bf80dc436762e63e9a131a3ad5f722c
Ben Cook <808@holbertonschool.com>
2020-10-29 20:17:44 -0700

    13 whoooo

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Counts words"""
+
+from collections import Counter, defaultdict
+import requests
+
+
+def count_words(subreddit, word_list, res=defaultdict(int), after=None):
+    """Count words"""
+    agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/86.0.4240.111 Safari/537.36"
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    if after:
+        url += f'?after={after}'
+    r = requests.get(
+        url,
+        headers={"User-Agent": agent},
+        allow_redirects=False
+    ).json()
+    for title in r.get('data').get('children'):
+        c = Counter(title.get('data').get('title').lower().split(' '))
+        for word in word_list:
+            if word.lower() in c:
+                res[word] += c.get(word.lower())
+    after = r.get('data').get('after')
+    if after:
+        return count_words(subreddit, word_list, res, after)
+    by_name = sorted(res.items(), key=lambda x: x[0])
+    for k, v in sorted(by_name, key=lambda x: x[1], reverse=True):
+        print(f'{k}: {v}')
